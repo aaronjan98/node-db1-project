cf894269202b28d0fce00040bc4c524cf88e8e6a
aaronjan <aaronjan98@gmail.com>
2020-02-17 14:53:43 -0800

    .delete added

--- routers/accounts/accounts-router.js
+++ routers/accounts/accounts-router.js
@@ -61,6 +61,22 @@ router.put("/:id", validateId, (req, res) => {
       });
 });
 
+router.delete('/:id', validateId, (req, res) => {
+    const id = req.params.id;
+
+    db('accounts')
+      .where({ id })
+      .del()
+      .then(count => {
+        res.status(200).json(count);
+      })
+      .catch(error => {
+        console.log(error);
+  
+        res.status(500).json({ error: "failed to remove the account" });
+      });
+  });
+
 // custom middleware
 
 function validateId(req, res, next) {
