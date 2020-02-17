6fba082a52168d8e94ff4ec03bff98b391da31df
aaronjan <aaronjan98@gmail.com>
2020-02-17 13:13:57 -0800

    ran npx knex seed:run
    and made .get to /api/accounts

--- server.js
+++ server.js
@@ -6,4 +6,21 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.send('<h3>DB Helpers with knex</h3>');
+})
+
+server.get("/api/accounts", (req, res) => {
+    db.select("*")
+      .from("accounts")
+      .then(accounts => {
+        res.status(200).json(accounts);
+      })
+      .catch(error => {
+        console.log(error);
+  
+        res.status(500).json({ error: "failed to get the list of accounts" });
+      });
+  });
+
 module.exports = server;
