f122c6bcfe929e29439f917784fb728770c4c177
aaronjan <aaronjan98@gmail.com>
2020-02-17 13:45:29 -0800

    made get request to a specific account

--- routers/accounts/accounts-router.js
+++ routers/accounts/accounts-router.js
@@ -5,7 +5,7 @@ const db = require("../../data/dbConfig.js");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    db.select("*")
+    db('*')
       .from("accounts")
       .then(accounts => {
         res.status(200).json(accounts);
@@ -15,6 +15,19 @@ router.get('/', (req, res) => {
   
         res.status(500).json({ error: "failed to get the list of accounts" });
       });
-  });
+});
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    db('accounts').where({ id })
+      .then(account => {
+        res.status(200).json(account);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "failed to get the account" });
+      });
+});
 
 module.exports = router;
