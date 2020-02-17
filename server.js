beafcb69cfade0dc15d373224df12ea3ac69a77e
aaronjan <aaronjan98@gmail.com>
2020-02-17 13:24:18 -0800

    created accounts-router.js

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const AccountsRouter = require('./routers/accounts/accounts-router.js');
 
 const server = express();
 
 server.use(express.json());
 
+server.use('/api/accounts', AccountsRouter);
+
 server.get('/', (req, res) => {
     res.send('<h3>DB Helpers with knex</h3>');
 })
-
-server.get("/api/accounts", (req, res) => {
-    db.select("*")
-      .from("accounts")
-      .then(accounts => {
-        res.status(200).json(accounts);
-      })
-      .catch(error => {
-        console.log(error);
-  
-        res.status(500).json({ error: "failed to get the list of accounts" });
-      });
-  });
 
 module.exports = server;
